4a144c4fd6dc0afff1939f12608896f0313b5e0b
gabmin <rlarkqals@gmail.com>
2025-12-14 22:03:58 +0900

    moitoring ui 패키지 생성

---
+++ bottail_monitoring/src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+
+</script>
+
+<template>
+  <RouterView />
+</template>
+
+<style scoped>
+@import './styles/reset.css';
+
+
+</style>
---
+++ bottail_monitoring/src/views/HomePage.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    홈 화면
+  </div>
+</template>
+
+<script lang="ts">
+
+</script>
+
+<style lang="scss">
+  // .
+</style>
