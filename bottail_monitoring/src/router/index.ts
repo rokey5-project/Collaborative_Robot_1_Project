885c626ae100f7636d34cb27acfb913d78869005
gabmin <rlarkqals@gmail.com>
2025-12-15 12:04:37 +0900

    feature/홈 메인화면 생성

--- bottail_monitoring/src/router/index.ts
+++ bottail_monitoring/src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomePage from '@/views/HomePage.vue'
+import OrderPage from '@/views/OrderPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,14 +10,11 @@ const router = createRouter({
       name: 'home',
       component: HomePage,
     },
-    // {
-    //   path: '/about',
-    //   name: 'about',
-    //   // route level code-splitting
-    //   // this generates a separate chunk (About.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () => import('../views/AboutView.vue'),
-    // },
+    {
+      path: '/orderPage',
+      name: 'orderPage',
+      component: OrderPage,
+    },
   ],
 })
 
--- bottail_monitoring/src/views/HomePage.vue
+++ bottail_monitoring/src/views/HomePage.vue
@@ -1,13 +1,54 @@
 <template>
-  <div>
-    홈 화면
+  <div class="home-background">
+    <div>
+      <img src="../assets/bottail_logo.png" alt="logo-img" class="home-logo-img"/>
+    </div>
+    <div class="home-order-button" @click="goToOrderPage">
+      주문하기
+    </div>
   </div>
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
+import router from '@/router';
+
+const goToOrderPage = () => {
+  router.push({ name: 'orderPage' });
+}
 
 </script>
 
 <style lang="scss">
-  // .
+  .home-background {
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    /* background-image: url('../assets/background.jpg');
+    background-size: contain; */
+
+
+    .home-logo-img {
+      width: 30vw;
+      height: 70vh;
+    }
+
+    .home-order-button {
+      width: 60vw;
+      height: 8vh;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      font-size: 32px;
+      font-weight: bold;
+      background-color: #B162EC;
+      color: #fff;
+      text-align: center;
+      border: none;
+      border-radius: 10px;
+    }
+  }
+
 </style>
